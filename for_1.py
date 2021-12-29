f8949c87b688de0ed48edb19af5472f53086f7c9
shubhm bhalerao <shubhambhalerao54312@gmail.com>
2021-12-29 14:52:08 +0530

    python_for loops

---
+++ for_1.py
@@ -0,0 +1,53 @@
+
+from typing import List
+
+
+x = [
+    {  'name':'kunal','age':23,'gender':'male','postion':'django inter'},
+    {'name':'shubham','age':21,'gender':'male','postion':'django intern'},
+    {'name':'arun','age':25,'gender':'male','postion':'django intern'},
+    {'name':'karan','age':21,'gender':'male','postion':'django intern'},
+    {'name':'shaheed','age':22,'gender':'male','postion':'django intern'},
+    {'name':'mayank','age':24,'gender':'male','postion':'django intern'},
+    {'name':'lakshit','age':25,'gender':'male','postion':'django intern'},
+    {'name':'suraj','age':24,'gender':'male','postion':'django intern'}   
+]
+Lst = []
+print(x)
+print(Lst)
+
+
+
+
+
+ # this is for delete the data in list\
+# for i in x :
+#     if i['name'] == 'shubham':
+#         x.remove(i)
+
+# print(x)
+
+# this is for when the data transfer one list to another list help of the append
+# for i in range(len(x)) :
+#     if x[i]['name'] == 'kunal':
+#         List.append(x[i])
+#         print(List)
+  
+
+
+
+#   this for find one person  the data of the list  
+    # if x[i]["name"] == "shubham":
+    #    print(x[i]['name'])    
+    #    print(x[i]['age'])    
+    #    print(x[i]['gender'])    
+    #    print(x[i]['postion'])    
+
+# this for when u find one single data all data has been delete it except by that single that mention in code 
+# for i in x:
+#     if i['name'] == 'shubham':
+#         Lst.append(i)
+#         x.clear()
+#         print(x)
+
+# print(Lst)
